5aca29a01fcd112304a71cf67ef7cc2cd7fbec25
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-03 16:30:27 -0500

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(d_names):
+    i=1
+    for name in d_names:
+        print(f'{i}. {name}')
+        i+=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [f'{el.capitalize()}!' for el in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for el in calls:
+        if (len(el) > 4):
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list): ##cheddar, gouda, camembert
+    for el in list:
+        if 'gouda' in el:
+            return 'gouda'
+        elif 'cheddar' in el:
+            return 'cheddar'
+        elif 'camembert' in el:
+            return 'camembert'
+    return None
